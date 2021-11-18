5c36937fef19e6ea7ea19e0cfb5d5bcfd77f95ce
Vinzenz Leutenegger <vleutenegger@gmail.com>
2021-11-18 18:43:31 +0100

    update: add recording type

--- components/audio.tsx
+++ components/audio.tsx
@@ -1,12 +1,13 @@
 import React, { useRef, useEffect } from 'react'
 // @ts-ignore
 import WaveSurfer from 'wavesurfer.js';
+import { Recording } from '../models/types';
 
 import audio from './audio.module.scss'
 
-export default function Audio({ recording }: { recording: any }) {
+export default function Audio({ recording }: { recording: Recording }) {
     const waveformRef = useRef(null);
-    
+
     useEffect(() => {
         if(waveformRef.current) {
             const wavesurfer = WaveSurfer.create({
